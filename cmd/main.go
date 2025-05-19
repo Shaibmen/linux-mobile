2f2f273d1ea61df8ed6b3f2a27996e5c8458e1e5
Shaibmen <ibin2005636@gmail.com>
2025-05-20 00:23:54 +0300

    log

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"log"
 	"server/environment"
+	"server/logging"
 	"server/server"
 )
 
+var Log *logging.FileLogger
+var err error
+
 func main() {
 
+	Log, err = logging.NewFileLogger("./app.log")
+	if err != nil {
+		log.Fatal("Ошибка инициализации логгера")
+	}
 	environment.InitEnv()
 	server.StartServer()
+
+	Log.Info("Сервер запущен")
 }
--- logging/logger.go
+++ logging/logger.go
@@ -1,9 +1,31 @@
-package logging 
+package logging
 
+import (
+	"log"
+	"os"
+)
 
-type Logger interface {
-	Debug(msg string)
-	Info(msg string)
-	Error(msg string)
+type FileLogger struct {
+	infoLog  *log.Logger
+	errorLog *log.Logger
 }
 
+func NewFileLogger(path string) (*FileLogger, error) {
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileLogger{
+		infoLog:  log.New(file, "[INFO]", log.LstdFlags),
+		errorLog: log.New(file, "[ERROR]", log.LstdFlags),
+	}, nil
+}
+
+func (f *FileLogger) Info(msg string) {
+	f.infoLog.Println(msg)
+}
+
+func (f *FileLogger) Error(msg string) {
+	f.errorLog.Println(msg)
+}
